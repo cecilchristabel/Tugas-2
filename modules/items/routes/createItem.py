24956324bd976f9577ca83105d2025037b8e6594
cecilchristabel <cecilia.cs0908@gmail.com>
2025-09-28 21:44:12 +0700

    Pesan commit awal

---
+++ modules/items/routes/createItem.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from typing import List
+from modules.items.schema.schemas import Item
+from modules.items.schema.schemas import ResponseModel
+
+router = APIRouter()
+items: List[Item] = []
+
+@router.post("/items/", response_model=ResponseModel)
+def create_item(item: Item):
+    items.append(item)
+    # return item
+    return {
+        "success": True,
+        "message": "New item successfully created",
+        "data": item
+    }
